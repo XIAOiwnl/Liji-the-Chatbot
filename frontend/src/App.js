3a7e41e6af9ed5d05cf6a9b830af1984591a5260
XIAOiwnl <kefan0308@gmail.com>
2023-04-23 21:07:01 -0400

    Update routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,19 +4,25 @@ import "./App.css";
 import Welcome from "./Welcome";
 import Questions from "./Questions";
 import Instructions from "./Instructions";
+import VanillaChat from "./VanillaChat";
+import LijiChat from "./LijiChat";
+import Complete from "./Complete";
 
 function App() {
-    return (
-        <Router>
-            <div className="App">
-                <Routes>
-                    <Route path="/" element={<Welcome />} />
-                    <Route path="/questions" element={<Questions />} />
-                    <Route path="/instructions" element={<Instructions />} />
-                </Routes>
-            </div>
-        </Router>
-    );
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/questions" element={<Questions />} />
+          <Route path="/instructions" element={<Instructions />} />
+          <Route path="/chatbot2" element={<VanillaChat />} />
+          <Route path="/chatbot1" element={<LijiChat />} />
+          <Route path="/complete" element={<Complete />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;

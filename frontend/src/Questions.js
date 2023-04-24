984a51dccc74aa0f80f47c64168ec3763a0548f6
XIAOiwnl <kefan0308@gmail.com>
2023-04-23 21:07:16 -0400

    Update Questions page

--- frontend/src/Questions.js
+++ frontend/src/Questions.js
@@ -1,51 +1,69 @@
-import React from 'react';
-import './Questions.css';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./Questions.css";
 
 function Questions() {
-  // Implement form submission logic here
+    const [answer1, setAnswer1] = useState("");
+    const [answer2, setAnswer2] = useState("");
+    const navigate = useNavigate();
 
-  return (
-    <div className="questions-page">
-      <h1>Predefined Questions</h1>
-      <form id="questions-form">
-      <div className="question">
-          <h3>Question 1</h3>
-          <label htmlFor="question1">What should I call you? </label>
-          <input name="question1" rows="4" cols="50" required />
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        // Store the predefined questions and answers
+        const predefinedQuestions = [
+            {
+                question: "How is the weather today?",
+                answer: answer1,
+            },
+            {
+                question: "What is your schedule look like for today?",
+                answer: answer2,
+            },
+        ];
+
+        // Navigate to LijiChat and pass the predefinedQuestions
+        navigate(`/chatbot1`, {
+            state: { predefinedQuestions },
+        });
+    };
+
+    return (
+        <div className="questions-page">
+            <h1>Predefined Questions</h1>
+            <form id="questions-form" onSubmit={handleSubmit}>
+                <div className="question">
+                    <h3>Question 1</h3>
+                    <label htmlFor="question1">
+                        How is the weather today?{" "}
+                    </label>
+                    <textarea
+                        name="question1"
+                        rows="4"
+                        cols="50"
+                        required
+                        value={answer1}
+                        onChange={(e) => setAnswer1(e.target.value)}
+                    />
+                </div>
+                <div className="question">
+                    <h3>Question 2</h3>
+                    <label htmlFor="question2">
+                        What is your schedule look like for today?{" "}
+                    </label>
+                    <textarea
+                        name="question2"
+                        rows="4"
+                        cols="50"
+                        required
+                        value={answer2}
+                        onChange={(e) => setAnswer2(e.target.value)}
+                    />
+                </div>
+                <input className="submit-btn" type="submit" value="Submit" />
+            </form>
         </div>
-        <div className="question">
-          <h3>Question 2</h3>
-          <label htmlFor="question3">How is the weather today? (Multiple Choice):</label>
-          <div className="radio-container">
-            <input type="radio" name="question3" value="option1" required /><label className="radio-label">Option 1</label>
-            <input type="radio" name="question3" value="option2" /><label className="radio-label">Option 2</label>
-            <input type="radio" name="question3" value="option3" /><label className="radio-label">Option 3</label>
-          </div>
-        </div>
-        <div className="question">
-          <h3>Question 3</h3>
-          <label htmlFor="question4">Are you busy recently? (Select Dropdown):</label>
-          <select name="question4" required>
-            <option value="">Select an option</option>
-            <option value="option1">Option 1</option>
-            <option value="option2">Option 2</option>
-            <option value="option3">Option 3</option>
-          </select>
-        </div>
-        <div className="question">
-          <h3>Question 4</h3>
-          <label htmlFor="question1">How do you feel today? </label>
-          <textarea name="question1" rows="4" cols="50" required />
-        </div>
-        <div className="question">
-          <h3>Question 5</h3>
-          <label htmlFor="question2">What's the most memorible thing that happended yesterday? </label>
-          <textarea name="question2" rows="4" cols="50" required />
-        </div>
-        <input className="submit-btn" type="submit" value="Submit" />
-      </form>
-    </div>
-  );
+    );
 }
 
 export default Questions;
